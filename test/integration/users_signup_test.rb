add7ddbf5421aaeb2f071080daec6349f86efbb4
Claudio Secco <claudiosecco@hotmail.com>
2020-05-12 20:07:16 -0300

    finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,4 +12,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select 'div#error_explanation'
   end
+
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: 'Test User',
+                                        email: 'testuser@example.com',
+                                        password: 'foobar',
+                                        password_confirmation: 'foobar' } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "div.alert-success"
+  end
+
 end
